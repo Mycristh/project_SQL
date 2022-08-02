15758057d9b9fb4831056e9ccefb7c6a0acab345
Mylena Oliveira <101020021+Mycristh@users.noreply.github.com>
2022-08-02 15:05:35 -0300

    Add files via upload

---
+++ inserindo dados.sql
@@ -0,0 +1,110 @@
+--inserindo dados na tabela livros
+insert into clube_do_livro.livros values (
+1, 'Percy Jackson e o Ladrão de Raios', 'Rick Riordan', 'intrínseca', 'Aventura', 34.65);
+
+insert into clube_do_livro.livros values (
+2, 'A Volta ao Mundo em 80 Dias', 'Júlio Verne', 'Principis', 'Aventura', 21.99);
+
+insert into clube_do_livro.livros values (
+3, 'O Cortiço', 'Aluísio de Azevedo', 'Panda Books', 'Romance', 47.8);
+
+insert into clube_do_livro.livros values (
+4, 'Dom Casmurro', 'Machado de Assis', 'Via Leitura', 'Romance', 19.90);
+
+insert into clube_do_livro.livros values (
+5, 'Memórias Póstumas de Brás Cubas', 'Machado de Assis', 'Antofágica', 'Romance', 45);
+
+insert into clube_do_livro.livros values (
+6, 'Quincas Borba', 'Machado de Assis', 'L&PM Editores', 'Romance', 48.5);
+
+insert into clube_do_livro.livros values (
+7, 'Ícaro', 'Gabriel Pedrosa', 'Ateliê', 'Poesia', 36);
+
+insert into clube_do_livro.livros values (
+8, 'Os Lusíadas', 'Luís Vaz de Camões', 'Montecristo', 'Poesia', 18.79);
+
+insert into clube_do_livro.livros values (
+9, 'Outros Jeitos de Usar a Boca', 'Rupi Kaur', 'Planeta', 'Poesia', 34.8);
+
+
+
+--inserindo dados na tabela vendedores
+insert into clube_do_livro.vendedores values(
+1,'Paula Rabelo');
+
+insert into clube_do_livro.vendedores values(
+2,'Juliana Macedo');
+
+insert into clube_do_livro.vendedores values(
+3,'Roberto Barros');
+
+insert into clube_do_livro.vendedores values(
+4,'Barbara Jales');
+
+--inserindo dados na tabela vendas
+insert into clube_do_livro.vendas values( 
+1, 3, 7, 1, '2020-11-02');
+
+insert into clube_do_livro.vendas values(
+2, 4, 8, 2, '2020-11-02');
+
+insert into clube_do_livro.vendas values(
+3, 4, 4, 3, '2020-11-02');
+
+insert into clube_do_livro.vendas values(
+4, 1, 7, 1, '2020-11-03');
+
+insert into clube_do_livro.vendas values(
+5, 1, 6, 3, '2020-11-03');
+
+insert into clube_do_livro.vendas values(
+6, 1, 9, 2, '2020-11-04');
+
+insert into clube_do_livro.vendas values(
+7, 4, 1, 3, '2020-11-04');
+
+insert into clube_do_livro.vendas values(
+8, 1, 5, 2, '2020-11-05');
+
+insert into clube_do_livro.vendas values(
+9, 1, 2, 1, '2020-11-05');
+
+insert into clube_do_livro.vendas values(
+10, 3, 8, 2, '2020-11-11');
+
+insert into clube_do_livro.vendas values(
+11, 1, 1, 4, '2020-11-11');
+
+insert into clube_do_livro.vendas values(
+12, 2, 10, 10, '2020-11-11');
+
+insert into clube_do_livro.vendas values(
+13, 1, 12, 5, '2020-11-18');
+
+insert into clube_do_livro.vendas values(
+14, 2, 4, 1, '2020-11-25');
+
+insert into clube_do_livro.vendas values(
+15, 3, 13, 2,'2021-01-05');
+
+insert into clube_do_livro.vendas values(
+16, 4, 13, 1, '2021-01-05');
+
+insert into clube_do_livro.vendas values(
+17, 4, 4, 3, '2021-01-06');
+
+insert into clube_do_livro.vendas values(
+18, 2, 12, 2, '2021-01-06');
+
+--inserindo dados na tabela estoque
+insert into clube_do_livro.estoque values(
+1,  7);
+
+insert into clube_do_livro.estoque values(
+2,  10);
+
+insert into clube_do_livro.estoque values(
+3,  2);
+
+insert into clube_do_livro.estoque values(
+8,  4);
